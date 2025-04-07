2ae7c10761fefc38d1378235f3c1a33370995cd6
KU-WM <ku990732@gmail.com>
2025-04-07 17:41:09 +0900

    test

--- salarytest/salary/models.py
+++ salarytest/salary/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    firstName = models.CharField(max_length=100)
+    lastName = models.CharField(max_length=100)
+    email = models.EmailField()
+    phoneNumber = models.CharField(max_length=15)
+    hireDate = models.DateField()
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    bonus = models.DecimalField(max_digits=10, decimal_places=2)
+    createAt = models.DateTimeField(auto_now_add=True)
